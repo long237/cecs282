1da4a09e347862d410ffb0aa268a83b9ac48d896
Long <ln24630@gmail.com>
2020-02-06 23:37:23 -0800

    fixing else if condition in the main

--- cs282/canon/canon/cannon_main.cpp
+++ cs282/canon/canon/cannon_main.cpp
@@ -12,33 +12,43 @@ int main(int argc, char* argv[]) {
 	double init_velocity;
 	double relative_dis = 2;
 	int seed;
-
-	cout << "Enter a seed value to start the game: " << endl;	seed = getSeed();
+	
+	//Prompt user to enter a seed value
+	seed = getSeed();
 	random_device rd;
 	mt19937 engine = mt19937 (seed);
 	uniform_real_distribution<double> tar_range{ 1.0 , 1000.0 };
 
+	//generate a random distance
 	tar_dis = tar_range(engine);
-	cout << "this is the target distance: " << tar_dis << endl;
+	cout << "this is the target distance: " << fixed << setprecision(2) << tar_dis << "m" <<endl;
+	cout << "" << endl;
+
 	while (relative_dis != 0) {
-		cout << "Enter the angle of fire below: " << endl;
 		usr_angle = getAngle();
-		
-		cout << "Enter the amount of gun powder below: " << endl;
+	
 		usr_powder = getPowder();
 		
 		init_velocity = calVelo(usr_powder);
 		usr_dis = calDistance(usr_angle, init_velocity);
 		relative_dis = hitCheck(tar_dis, usr_dis);	
+
+		//cout << "target: " << tar_dis << endl;
+		//cout << "user: " << relative_dis << endl;
 		
+		//Print target hit if the bomb relative distance to the target is within 1m
 		if(relative_dis == 0) {
 			cout << "Target Hit!!!!!" << endl;
 		}
-		else if (relative_dis > tar_dis) {
-			cout << "You overshot by: " << fixed << setprecision(2) << relative_dis<< " m"<< endl;
+		//Print overshot if the user distance is more than the target distance
+		else if (usr_dis > tar_dis) {
+			cout << "You overshot by: " << fixed << setprecision(2) << relative_dis<< "m"<< endl;
+			cout << "" << endl;
 		}
+		//Print short if the user distance is smaller than the target distance
 		else {
-			cout << "You are short by: " << fixed << setprecision(2) << relative_dis << " m"<<endl;
+			cout << "You are short by: " << fixed << setprecision(2) << relative_dis << "m"<<endl;
+			cout << "" << endl;
 		}
 	}
 }
--- cs282/canon/canon/firing_funct.cpp
+++ cs282/canon/canon/firing_funct.cpp
@@ -4,42 +4,41 @@
 #include <cmath>
 using namespace std;
 
-//int seed;
-//random_device rd;
-//mt19937 engine = mt19937 (seed);
-//uniform_int_distribution<double> tar_range{ 1.0 , 1000.0 };
-
+//Get the seed from the user, keep asking until a positive value is entered
 int getSeed() {
 	int seedValue = -1;
 	
-	while (seedValue < 0) {	
+	while (seedValue <= 0) {
+		cout << "Enter a seed value to start the game: " << endl;
 		cin >> seedValue;
 	}
 	return seedValue;
 }
 
-//double getTarget() {	
-//	return tar_range(engine);
-//}
-
+//Prompt the user for the firing angle and continue until the user enter a 
+//value between 0 < x < 90
 double getAngle() {
 	double angleValue = -1;
 	while (angleValue < 0 || angleValue > 90) {
+		cout << "Enter the angle of fire below: " << endl;
 		cin >> angleValue;
 	}
 	return angleValue;
 } 
 
+//Prompt the user for the amount of powder to fire the canon
 double getPowder() { 
 	double powderValue = -1;
 	while (powderValue < 0) {
+		cout<< "Enter the amount of gun powder below: " << endl;
 		cin >> powderValue;
 	}
 	return powderValue;
 }
 
+//Calculate the distance of the bomb with angle and velocity
 double calDistance(double angle, double velocity) {
-	double gravity = -9.8;
+	double gravity = -9.81;
 	double v_vertical;
 	double v_horizontal;
 	double pi = 3.141592653589793238463;
@@ -48,9 +47,13 @@ double calDistance(double angle, double velocity) {
 	double rad_angle = angle * (pi / 180);	
 	v_vertical = velocity * sin(rad_angle);
 
+	//calculating horizontal velocity
 	v_horizontal = velocity * cos(rad_angle);
 
+	//calculating time it takes for the bomb to return to ground from the apex
 	double t_apex = (- v_vertical / gravity);
+
+	//calculate the horizontal distance traveled by the bomb
 	double horizontal_distance = v_horizontal * 2 * t_apex;
 	
 	return horizontal_distance;
@@ -67,10 +70,12 @@ double hitCheck(double target_distance, double usr_distance) {
 	}
 	else if (usr_distance > target_distance) {
 		double over_dis = usr_distance - target_distance;
+		//cout << "user: " << usr_distance << endl;
 		return over_dis; 
 	}
 	else {
-		double short_dis = usr_distance - target_distance;  
+		double short_dis = target_distance - usr_distance; 
+		//cout << "user2: " << usr_distance << endl;
 		return short_dis;
 	}
 }
