80ebc7fc124d117096b36820c5beb968f7e4383a
Long <ln24630@gmail.com>
2020-02-10 20:51:52 -0800

    adding headers for tic tac toe

--- cs282/lab/lab1.cpp
+++ cs282/lab/lab1.cpp
@@ -1,22 +1,22 @@
 #include <iostream>;
 using namespace std;
 int RoundtoNearest(double x);
-int main(int argc, char* argv[]) {
-	double x = 2.9;
-	cout << "this is the result " << RoundtoNearest(x) << endl;
-
-	int a = 9;
-	int b = a;
-	int *c = &a;
-	int *d = c;
-	int e = *c;
-
-	cout << "a" << a << endl;
-	cout << "b" << b << endl;
-	cout << "c" << c << endl;
-	cout << "d" << d << endl;
-	cout << "e" << e << endl;
-}
+//int main(int argc, char* argv[]) {
+//	double x = 2.9;
+//	cout << "this is the result " << RoundtoNearest(x) << endl;
+//
+//	int a = 9;
+//	int b = a;
+//	int *c = &a;
+//	int *d = c;
+//	int e = *c;
+//
+//	cout << "a" << a << endl;
+//	cout << "b" << b << endl;
+//	cout << "c" << c << endl;
+//	cout << "d" << d << endl;
+//	cout << "e" << e << endl;
+//}
 
 int RoundtoNearest(double x) {
 	int y = x + 0.5;
---
+++ cs282/lab/lab3.cpp
@@ -0,0 +1,52 @@
+#include <iostream>;
+#include <cmath>;
+using namespace std;
+int SolveQuadratic(const double a, const double b, const double c,  double *xSolution1, double *xSolution2) {
+	double delta = pow( b, 2) - (4 * a * c);
+	if ( delta < 0 ) {
+		*xSolution1 = 0;
+		*xSolution2 = 0;
+		return 0;
+	}
+	else {
+		*xSolution1 = (-b + sqrt(delta)) / (2 * a);
+		*xSolution2 = (-b - sqrt(delta)) / (2 * a);
+
+		if (*xSolution1 == *xSolution2) {
+			*xSolution2 = 0.0;
+			return 1;
+		}
+		else {
+			return 2;
+		}
+	}
+}
+int main(int argc, char* argv[]) {
+	double a = 1;
+	double b = 2;
+	double c = 1;
+	double xSolution1, xSolution2;
+	int numSolution = SolveQuadratic(a ,b , c, &xSolution1, &xSolution2);
+	cout << "Solution 1: " << xSolution1 << endl;
+	cout << "Solution 2: " << xSolution2 << endl;
+	cout << "number of solutions: "<< numSolution <<endl;
+	cout << "" <<endl;
+	a = 1;
+	b = 0;
+	c = 1;
+	numSolution = SolveQuadratic(a ,b , c, &xSolution1, &xSolution2);
+	cout << "Solution 1: " << xSolution1 << endl;
+	cout << "Solution 2: " << xSolution2 << endl;
+	cout << "number of solutions: "<< numSolution <<endl;
+	cout << "" <<endl;
+	a = 1;
+	b = -3;
+	c = -4;
+	numSolution = SolveQuadratic(a ,b , c, &xSolution1, &xSolution2);
+	cout << "Solution 1: " << xSolution1 << endl;
+	cout << "Solution 2: " << xSolution2 << endl;
+	cout << "number of solutions: "<< numSolution <<endl;
+
+}
+
+
