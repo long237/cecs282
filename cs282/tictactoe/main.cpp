0f9e2dbcbf194616122aa03c16b5967b6b2eab7d
Long <ln24630@gmail.com>
2020-02-11 22:43:18 -0800

    testing function with main

--- cs282/tictactoe/main.cpp
+++ cs282/tictactoe/main.cpp
@@ -10,21 +10,32 @@ int main(int argc, char* argv[]) {
 	GetMove(usr_row, usr_col);
 	cout<< "this is row: " << usr_row <<endl;
 	cout << "this is col: " << usr_col <<endl;
-
+	
 	char test = 5;
 	cout<< "char value " << test << endl;
-	
+	cout<<"test array" <<endl;	
+	std::array<char,3> testcase = {0,0,0};
+	for(int i = 0; i < 3; i++) {
+		cout<< testcase[i] <<endl;
+	}
+	char test_var = '-1';
+	cout << "this is test variable: " << (int) test_var << endl;
 	std::array<std::array<char, 3>, 3> board = {0};
-	
+	//cout << "this is the array: " << board << endl;	
+	PrintBoard(board);	
+
 	board[0][0] = 0;
 	board[0][1] = 0;
 	board[0][2] = '1';
 	board[1][0] = 0;
-	board[1][1] = '-1';
+	board[1][1] = '1';
 	board[1][2] = 0;
 	board[2][0] = '1';
 	board[2][1] = 0;
-	board[2][2] = '-1';
+	board[2][2] = '1';
 	cout<<"this is the board: "<< endl;
 	PrintBoard(board);
+
+	cout<< "test check move " << MoveIsValid(board, usr_row, usr_col)<< endl;
+ 
 }
--- cs282/tictactoe/xAndOfunct.cpp
+++ cs282/tictactoe/xAndOfunct.cpp
@@ -4,16 +4,19 @@
 using namespace std;
 
 void PrintBoard(const std::array<std::array<char, 3>, 3> &board){
+	cout << "- 0 1 2 "<< endl;
 	for (int i = 0; i < 3; i++) {
+		cout<< i << " ";
 		for (int j = 0; j < 3; j++) {
 			if (board[i][j] == '1') {
-				cout<< "X";
+				cout<< "X ";
 			}
 			else if (board[i][j] == '-1'){
-				cout<< "O";
+				cout<< "O ";
 			} 
 			else {
-				cout<< ".";
+				cout<< ". ";
+				//cout << board[i][j];
 			}
 		}
 		cout<< ""<<endl;
@@ -22,14 +25,26 @@ void PrintBoard(const std::array<std::array<char, 3>, 3> &board){
 
 //take in references and modified them.
 void GetMove(int &row, int &col){
+	char dummy_var;
 	cout<< "Enter the row and column to place a symbol: "<< endl;
-	cin>> row;
-	cin>> col;
+	cin>> row >> dummy_var >> col;
 	
 }
 
 bool  MoveIsValid(const std::array<std::array<char, 3>, 3> &board,
-		int row, int col);
+		int row, int col) {
+	if ((row >= 0 && row < 3) && (col >= 0 && col < 3)) {
+		if ( board[row][col] == 0) {
+			cout<< "def true"<< endl;
+			return true;
+		}
+	}
+	else {
+		cout<< "def false" << endl;
+		return false;
+	}
+}
+
 
 char CheckWinner(const std::array<std::array<char, 3>, 3> &board);
 
