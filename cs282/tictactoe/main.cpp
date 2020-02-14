51ebdc94fecc064b436766a6184ed543d2b5c35a
Long <ln24630@gmail.com>
2020-02-13 17:50:44 -0800

    add checkwinner function and implement into main

--- cs282/tictactoe/main.cpp
+++ cs282/tictactoe/main.cpp
@@ -15,39 +15,61 @@ int main(int argc, char* argv[]) {
     game_board[2][1] = 0;
     game_board[2][2] = 0;
 
-    char test1 = -1;
-    char test2 = 1;
-    if (test1 == test2) {
-        cout << "they are equal" << endl;
-    }
+    //cout << "winner value" << CheckWinner(game_board) << endl;
 
-    if (test2 == 1) {
-        cout << "1 char is equal to 1 char" << endl;
-    }
 
     int turn = 1;
     int row;
     int col;
     bool validity = false;
-    //keep asking for input if moveisvalid return false
+    int winner;
+    //Run the game for 9 loops.
     for (int i = 0; i < 9; i++) {
         PrintBoard(game_board);
         bool validity = false;
+
+        //Check for the winner at every iteration
+        winner = CheckWinner(game_board);
+        if (winner != 0) {
+            break;
+        }
+
+        cout << "winner value: " << winner << endl;
+        //cheking for the user input
         while (!validity) {
-            cout << "Player" << turn << endl;
+            if (turn == 1) {
+                cout << "X's turn "<< endl;
+            }
+            else {
+                cout << "O's turn" << endl;
+            }
             GetMove(row, col);
             validity = MoveIsValid(game_board, row, col);
         }
+
+        //change the elements value in the game board.
+        //change to 1 if it is player X turn and decrease the counter
         if (turn == 1) {
             game_board[row][col] = 1;
-            cout << "player" << turn << "move" << endl;
-            turn++;
+            turn = turn + 1;
         }
+        //change elements to -1 and increase counter turn.
         else {
             game_board[row][col] = -1;
-            cout << "player" << turn << "move" << endl;
             turn = turn - 1;
         }
     }
 
+    //check the board again in case of winning on last move
+    winner = CheckWinner(game_board);
+    PrintBoard(game_board);
+    if (winner == 1) {
+        cout << "Player X wins" << endl;
+    }
+    else if (winner == -1){
+        cout << "Player O wins" << endl;
+    }
+    else {
+        cout << "Draw !!!!" << endl;
+    }
 }
--- cs282/tictactoe/xAndOfunct.cpp
+++ cs282/tictactoe/xAndOfunct.cpp
@@ -8,15 +8,17 @@ void PrintBoard(const std::array<std::array<char, 3>, 3> &board){
 	for (int i = 0; i < 3; i++) {
 		cout<< i << " ";
 		for (int j = 0; j < 3; j++) {
+			//Print X if the element is a 1
 			if (board[i][j] == 1) {
 				cout<< "X ";
 			}
+			//else print O if the element is a -1
 			else if (board[i][j] == -1){
 				cout<< "O ";
 			} 
+			//Print a "." if the element is a zero.
 			else {
 				cout<< ". ";
-				//cout << board[i][j];
 			}
 		}
 		cout<< ""<<endl;
@@ -31,47 +33,51 @@ void GetMove(int &row, int &col){
 	
 }
 
-bool  MoveIsValid(const std::array<std::array<char, 3>, 3> &board,
+bool MoveIsValid(const std::array<std::array<char, 3>, 3> &board,
 		int row, int col) {
+	//If the move is inside the matrix and the element is equal to zero, return true.
 	if ((row >= 0 && row < 3) && (col >= 0 && col < 3) && (board[row][col] == 0)) {
 			return true;
 	}
 	else {
-		//cout<< "def false" << endl;
 		return false;
 	}
 }
 
 //return the value 1 or -1 if there are 3 matching.
-char CheckWinner(const std::array<std::array<char, 3>, 3>& board){
+char CheckWinner(const std::array<std::array<char, 3>, 3>& board) {
 	//check if for the horizontal line matching.
-	if (board[0][0] == board[0][1] == board[0][2]) {
+	if ((board[0][0] == board[0][1]) && (board[0][0]== board[0][2])) {
 		return board[0][0];
 	}
-	else if (board[1][0] == board[1][1] == board[1][2]) {
+	else if ((board[1][0] == board[1][1]) && (board[1][0] == board[1][2])) {
 		return board[1][0];
 	}
-	else if (board[2][0] == board[2][1] == board[2][2]) {
+	else if ((board[2][0] == board[2][1]) && (board[2][0] == board[2][2])) {
 		return board[2][0];
 	}
 
 	//check for vertical line matching
-	else if (board[0][0] == board[1][0] == board[1][0]) {
+	else if ((board[0][0] == board[1][0]) && (board[0][0] == board[2][0])) {
 		return board[0][0];
 	}
-	else if (board[0][1] == board[1][1] == board[2][1]) {
+	else if ((board[0][1] == board[1][1]) && (board[0][1] == board[2][1])) {
 		return board[0][1];
 	}
-	else if (board[0][2] == board[1][2] == board[2][2]) {
+	else if ((board[0][2] == board[1][2]) && (board[0][2] == board[2][2])) {
 		return board[0][2];
 	}
 
 	//check for diagonal line matching
-	else if (board[0][0] == board[1][1] == board[2][2]) {
+	else if ((board[0][0] == board[1][1]) && (board[0][0] == board[2][2])) {
 		return board[0][0];
 	}
-	else if (board[0][2] == board[1][1] == board[2][0]) {
+	else if ((board[0][2] == board[1][1]) && (board[0][2] == board[2][0])) {
 		return board[0][2];
+	}
+	//return 0 if there are no matches. 
+	else {
+		return 0;
 	}
 }
 
