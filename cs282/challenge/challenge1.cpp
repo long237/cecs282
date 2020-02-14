2b6211822db17a934f8fec04ab4e363bb20735b4
Long <ln24630@gmail.com>
2020-02-14 10:23:03 -0800

    change the loop duration

--- cs282/challenge/challenge1.cpp
+++ cs282/challenge/challenge1.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
-	for (int i = -1; i < 4; i++) {
-		for (int j = -1; j < 4; j++) {
+	for (int i = -1; i < 5; i++) {
+		for (int j = -1; j < 5; j++) {
 			cout << i << " " << j << endl;
 		}
 	}
