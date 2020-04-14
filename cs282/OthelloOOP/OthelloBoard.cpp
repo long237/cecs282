b640ef3fefcf7c1e0cb87e5c35b3d6b39530f7f4
Neo <ln24630@gmail.com>
2020-04-14 11:16:13 -0700

    Initializing GetPossibleMove

--- cs282/OthelloOOP/OthelloBoard.cpp
+++ cs282/OthelloOOP/OthelloBoard.cpp
@@ -1,4 +1,6 @@
 #include "OthelloBoard.h"
+#include <vector>
+using namespace std;
 
 OthelloBoard::OthelloBoard() {
 	mBoard = {Player::EMPTY};
@@ -8,4 +10,23 @@ OthelloBoard::OthelloBoard() {
 	mBoard[4][3] = Player::BLACK;
 	mCurrentPlayer = Player::BLACK;
 	mCurrentValue = 0;
+}
+
+vector<unique_ptr<OthelloMove>> OthelloBoard::GetPossibleMoves() const {
+	//Iterate the entire board
+	for (int row = 0; row < BOARD_SIZE; row++) {
+		for (int col = 0; col < BOARD_SIZE; col++) {
+
+			BoardPosition tempPosition = BoardPosition(row, col);
+			//checking all of the direction in the Cardinal direction list
+			for (int direction = 0; direction < BoardDirection::CARDINAL_DIRECTION.size(); direction++) {
+				//Move to the first position of that direction to check
+				BoardPosition posInOneDirection = tempPosition + BoardDirection::CARDINAL_DIRECTION[direction];
+
+				while (InBounds(posInOneDirection) && (mBoard[posInOneDirection.GetRow][posInOneDirection.GetCol] != Player::EMPTY) {
+
+				}
+			}
+		}
+	}
 }
